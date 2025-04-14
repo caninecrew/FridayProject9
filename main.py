905c78c589fa467c762687d4c244f726286d867f
caninecrew <samuelleerumbley@gmail.com>
2025-04-14 09:59:48 -0500

    Fix setup_openai method to use instance variable for OpenAI client

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ class OpenAIGUI(QMainWindow):
         super().__init__()
         
     def setup_openai(self):
-
         load_dotenv()
 
         api_key = os.getenv("OPENAI_API_KEY")
-        client = OpenAI(api_key=api_key)
+        self.client = OpenAI(api_key=api_key)
 
 completion = client.chat.completions.create(
     model="gpt-4o",
